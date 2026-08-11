8f01c3bb133595977ced17de98d01ddcd58a33a6
khaidir16 <150641846+khaidir16@users.noreply.github.com>
2026-08-11 23:02:47 +0800

    Add core application constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,7 @@
+abstract final class AppConstants {
+  static const String appName = 'EduTech SMK';
+  static const String appTagline = 'Sistem Manajemen Pembelajaran Digital';
+  static const String phaseOneLabel = 'Fondasi proyek siap';
+
+  const AppConstants._();
+}
